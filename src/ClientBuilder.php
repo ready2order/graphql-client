a4fe0381b370f063d0079c3d7fa4f96c326adbe2
Christopher Fuchs <christopher.fuchs@ready2order.com>
2018-02-07 10:45:10 +0100

    #6 Added possibility to provide custom guzzle options
    * added phpdocs for ClientBuilder

--- src/ClientBuilder.php
+++ src/ClientBuilder.php
@@ -7,23 +7,40 @@ use Psr\Cache\CacheItemPoolInterface as Cache;
 
 class ClientBuilder
 {
-    public static function build(string $endpoint)
-    {
-        return new \Softonic\GraphQL\Client(
-            new \GuzzleHttp\Client(['base_uri' => $endpoint]),
-            new \Softonic\GraphQL\ResponseBuilder()
-        );
-    }
+	/**
+	 * @param string $endpoint
+	 * @param array $guzzleOptions
+	 * @return Client
+	 */
+	public static function build(string $endpoint, array $guzzleOptions = []) : Client
+	{
+		// allow custom guzzle options
+		$guzzleOptions = array_merge(['base_uri' => $endpoint], $guzzleOptions);
 
+		return new \Softonic\GraphQL\Client(
+			new \GuzzleHttp\Client($guzzleOptions),
+			new \Softonic\GraphQL\ResponseBuilder()
+		);
+	}
+
+	/**
+	 * @param string $endpoint
+	 * @param OAuth2Provider $oauthProvider
+	 * @param array $tokenOptions
+	 * @param Cache $cache
+	 * @param array $guzzleOptions
+	 * @return Client
+	 */
     public static function buildWithOAuth2Provider(
         string $endpoint,
         OAuth2Provider $oauthProvider,
         array $tokenOptions,
-        Cache $cache
+        Cache $cache,
+	    array $guzzleOptions = []
     ): Client {
-        $guzzleOptions = [
-            'base_uri' => $endpoint,
-        ];
+		// allow custom guzzle options
+	    $guzzleOptions = array_merge(['base_uri' => $endpoint], $guzzleOptions);
+
 
         return new \Softonic\GraphQL\Client(
             \Softonic\OAuth2\Guzzle\Middleware\ClientBuilder::build(
